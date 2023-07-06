b223aeedefa961442868213ac18118603bba8aff
ElMoNoW <75977599+ElMoNoW@users.noreply.github.com>
2023-07-06 20:50:27 +0700

    Update secrets.py

--- secure/secrets.py
+++ secure/secrets.py
@@ -1,1 +1,6 @@
+# Sensitive information and secrets
 
+DISCORD_TOKEN = 'your_discord_token_here'
+API_SECRET = 'your_api_secret_here'
+
+# In the secrets.py file, you can store sensitive information and secrets required for your bot. In this example, we have included the Discord bot token (DISCORD_TOKEN) and an API secret key (API_SECRET). Replace the placeholder values with your actual token and secret key.
