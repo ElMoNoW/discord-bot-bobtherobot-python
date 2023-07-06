2192946e3da1919a5c97794497ae2d15da5043de
ElMoNoW <75977599+ElMoNoW@users.noreply.github.com>
2023-07-06 20:49:42 +0700

    Update config.py

--- secure/config.py
+++ secure/config.py
@@ -1,1 +1,7 @@
+# Configuration settings
 
+PREFIX = '!'
+DATABASE_URL = 'your_database_url_here'
+API_KEY = 'your_api_key_here'
+
+# In the config.py file, you can define various configuration settings for your Discord bot. In this example, we have defined the prefix used for commands (PREFIX), the database URL (DATABASE_URL), and an API key (API_KEY). Modify the values accordingly to match your specific configuration.
